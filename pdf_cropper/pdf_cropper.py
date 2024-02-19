4a253af2d01b650da29f57300350270816474d6c
GoldTitaniumSulfurCarbon <95988349+GoldTitaniumSulfurCarbon@users.noreply.github.com>
2024-02-19 15:55:20 -0600

    Add files via upload
    Updated version where the user can choose to keep running the program instead of one-time.

--- pdf_cropper/pdf_cropper.py
+++ pdf_cropper/pdf_cropper.py
@@ -1,31 +1,42 @@
 from PyPDF2 import PdfReader, PdfWriter
 from pathlib import Path
+
 # TODO: Allow user to enter path of desired folder through user input
-# TODO: Allow user to enter path of where outputs should be, if they do not want to use the default one.
-file_directory = Path("E:\Desktop\Calculus Transcendentals 8E.pdf") # Creates path to desired pdf
-output_directory = Path("outputs") # Output path in the folder of this script is created.
+def pdfCropper(directory):
+    file_directory = Path(directory) # Creates path to desired pdf
+    output_directory = Path("outputs") # Output path in the folder of this script is created.
 
-if not output_directory.exists(): # Creates the aforementioned folder if it does not already exist.
-    output_directory.mkdir()
-    print("Output folder has been created in the folder this script is located.")
+    if not output_directory.exists(): # Creates the aforementioned folder if it does not already exist.
+        output_directory.mkdir()
+        print("Output folder has been created in the folder this script is located.")
 
-if file_directory.exists():
-    input_pdf = PdfReader(file_directory)
-    cropped_pdf = PdfWriter()
-    print(f"The given PDF has {len(input_pdf.pages)} total pages.")
+    if file_directory.exists():
+        input_pdf = PdfReader(file_directory)
+        cropped_pdf = PdfWriter()
+        print(f"The given PDF has {len(input_pdf.pages)} total pages.")
 
-    starting_point = int(input("Please put in the index you want to begin the cropping at"))
-    ending_point = int(input("Please put in the indes you want to end the cropping at"))
+        starting_point = int(input("Please put in the index you want to begin the cropping at"))
+        ending_point = int(input("Please put in the index you want to end the cropping at"))
 
-    for page in input_pdf.pages[starting_point-1:ending_point]: # starting_point is decreased by one to account for that page to begin at.
-        cropped_pdf.add_page(page)
+        for page in input_pdf.pages[starting_point-1:ending_point]: # starting_point is decreased by one to account for that page to begin at.
+            cropped_pdf.add_page(page)
 
-    cropped_pdf_name = input("What do you want to name the cropped pdf? File extension not needed")
-    cropped_pdf_name += ".pdf"
-    output_file = output_directory / cropped_pdf_name
-    with output_file.open(mode="wb") as output:
-        cropped_pdf.write(output)
-    print(f"Success! You can find your file {cropped_pdf_name} inside the \"outputs\" folder located where this script is.")
+        cropped_pdf_name = input("What do you want to name the cropped pdf? File extension not needed")
+        cropped_pdf_name += ".pdf"
+        output_file = output_directory / cropped_pdf_name
+        with output_file.open(mode="wb") as output:
+            cropped_pdf.write(output)
+        print(f"Success! You can find your file {cropped_pdf_name} inside the \"outputs\" folder located where this script is.")
 
-else:
-    raise Exception("The given path is not valid")
+    else:
+        raise Exception("The given path is not valid")
+
+
+
+# User input if they want to keep going.
+file_directory = "E:\Desktop\Calculus Transcendentals 8E.pdf"
+user_input = "y"
+while user_input == "y" :
+    pdfCropper(file_directory)
+    user_input = input("Do you want to keep running this? \"y\" for yes, and any other for no.")
+    user_input = user_input.casefold()
